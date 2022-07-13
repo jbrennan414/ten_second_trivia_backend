8a9fc42120087215782b3785cf2a3fd8f74d8026
John Brennan <brennanj414@gmail.com>
2022-07-13 17:29:42 -0600

    add auth

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require('express')
+require('dotenv').config()
+
+const express = require('express');
 const app = express()
 const port = 3000
-var get_questions = require('./get_methods');
 
+const get_questions = require('./get_methods');
 
 var securedRoutes = require('express').Router()
 
@@ -11,20 +13,18 @@ app.get('/', (req, res) => {
 })
 
 function middleware(req, res, next) {
-  console.log(req.headers.login)
 
-  const login = req.headers.login
-  const password = req.headers.password
+  // hash login/password client side, send as an auth header
+  console.log(req.headers.authorization)
+  console.log(process.env.BASIC_AUTH)
 
-  console.log(Buffer.from(`${login}:${password}`).toString('base64'));
+  // Verify credentials
+  if (req.headers.authorization !== process.env.BASIC_AUTH) {        
+    return res.status(401).send('Authentication required.') // Access denied.   
+  }
 
-    // Verify credentials
-  // if (req.headers.authorization !== 'c3dhZ2dnZ2dnZzpmb29iYXI='
-  //     && req.headers.authorization !== 'Basic b3RoZXJsb2dpbjpvdGhlcnBhc3N3b3Jk')        
-  //   return res.status(401).send('Authentication required.') // Access denied.   
-
-    // Access granted...
-    next()
+  // Access granted...
+  next()
 }
 
 securedRoutes.use(middleware)
@@ -34,7 +34,6 @@ securedRoutes.get('/question', (req, res) => {
   res.send(response)
 
 }) 
-
 
 app.use('/secure', securedRoutes)
 app.get('public', /* ... */)
