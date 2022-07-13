7deacd932048b05b3b869e689277da3dabbf4e26
John Brennan <brennanj414@gmail.com>
2022-07-13 17:48:46 -0600

    add mongo call

--- get_methods.js
+++ get_methods.js
@@ -4,25 +4,21 @@ var url = "mongodb://localhost:27017/";
 module.exports = {
     get_question: function () {
 
-    //     MongoClient.connect(url, function(err, db) {
-    //         if (err) {
-    //             return err
-    //         }
-    //         var dbo = db.db("questions");
-    //         dbo.collection("questions").findOne({}, function(err, result) {
-    //             if (err) {
-    //                 return err
-    //             }
+        MongoClient.connect(url, function(err, db) {
+            if (err) {
+                return `error ${err}`
+            }
+            var dbo = db.db("questions");
+            dbo.collection("questions").findOne({}, function(err, result) {
+                if (err) {
+                    return `error ${err}`                
+                }
               
-    //             db.close();
+                db.close();
 
-    //             return result.name
+                return result.name
 
-    //         });
-    //     });
-
-
-        return "ok that's right"
-
+            });
+        });
     }
   };
