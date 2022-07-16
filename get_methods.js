49e53d2fb23c110c2facddfaebd25802f622ae36
John Brennan <brennanj414@gmail.com>
2022-07-16 16:19:20 -0600

    fix db conn

--- get_methods.js
+++ get_methods.js
@@ -1,16 +1,18 @@
 require("dotenv").config()
 
 var MongoClient = require('mongodb').MongoClient;
-// var dbUrl = 'mongodb://127.0.0.1:27017/';
 
-var dbUrl = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@127.0.0.1:27017/`
+var targetDB = "questions"
+var host = "localhost"
+
+var dbUrl = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@${host}/${targetDB}`
 
 const get_question = _ => {
     return new Promise((resolve => {
 
         MongoClient.connect(dbUrl, function(err, client) {
             if (err) {
-                resolve("this error", err)
+                resolve(`${err}`)
                 throw err;
             } else {
                 console.log("WE CONNECTED")
