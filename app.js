bf71e3e18baf060ba0a9661a45d11e68840202e9
John Brennan <brennanj414@gmail.com>
2022-07-17 16:58:27 -0600

    dynamically populate date

--- app.js
+++ app.js
@@ -37,7 +37,9 @@ const asyncFunc = (text) => {
 }
 
 securedRoutes.get('/question', asyncHandler(async (req, res) => {
-  const result1 = await get_methods.get_question("160722")
+  let dates = req.params.date
+
+  const result1 = await get_methods.get_question(dates)
   // const [result2, result3] = await Promise.all([
   //   get_methods.get_question(),
   //   asyncFunc('my name is'),
