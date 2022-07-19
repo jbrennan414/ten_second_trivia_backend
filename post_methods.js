cacb9c4f70cbe0a0e571512454316316d77ea9db
John Brennan <brennanj414@gmail.com>
2022-07-18 21:34:29 -0600

    post method

--- post_methods.js
+++ post_methods.js
@@ -9,51 +9,25 @@ var dbUrl = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@$
 
 const axios = require('axios');
 
-const post_new_question = async () => {
-    console.log("post new question")
-    axios
-        .get('https://the-trivia-api.com/api/questions?limit=5&difficulty=easy')
-        .then(res => {
-            console.log(`statusCode: ${res.status}`);
-            console.log("resonse from trivia...", res);
-
-            await postToMongo(res)
-
-        })
-        .catch(error => {
-            console.error(error);
-        });
-}
-
-const postToMongo = async (todayquestion) => {
-
-    console.log("tryna post to mongo")
-
+// const post_new_question = async () => {
+const post_new_question = _ => {
     return new Promise((resolve => {
 
-        MongoClient.connect(dbUrl, function(err, client) {
-            if (err) {
-                resolve(`${err}`)
-                throw err;
-            } else {
-                console.log("WE CONNECTED")
+        console.log("post new question")
+        axios
+            .get('https://the-trivia-api.com/api/questions?limit=5&difficulty=easy')
+            .then(res => {
+                console.log(`statusCode: ${res.status}`);
+                console.log("resonse from trivia...", res);
 
-                const db = client.db('questions');
-                const questionsCollection = db.collection("questions");
-
-                const doc = {
-                    date: "180722",
-                    question: todayquestion,
-                }
-
-                const ourResult = await questionsCollection.insertOne(doc);
-
-                resolve(ourResult)
-            }
-        })
+                await postToMongo(res)
+                resolve()
+            })
+            .catch(error => {
+                console.error(error);
+                resolve()
+            });
     }))
 }
-
-
 
 module.exports = { post_new_question: post_new_question }
